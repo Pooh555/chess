5da95663f821c34a9b6c82edcd61aba03520abd1
Pooh555 <peerasit.lu@gmail.com>
2024-12-29 18:56:41 +0700

    Create game lopp

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,16 +1,57 @@
 package main;
 
-import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     public static final int WIDTH = 1100; // Panel's width
-    public static final int HEIGHT = 500; // Panel's height
+    public static final int HEIGHT = 800; // Panel's height
     public static final Color BACKGROUND_COLOR = new Color(0, 0, 0); // Background color
+    public int FPS = 360; // FPS
+
+    Thread gameThread;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(BACKGROUND_COLOR);
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        
+        // Game loop
+        double drawInterval = 1000000000/FPS; // 1 tick
+        double delta = 0; // How much time hass passed since the last render
+        long lastTime = System.nanoTime(); // Time of the last render
+        long currentTime; // Current time
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime)/drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+
+    private void update() {
+
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,14 @@ public class Main {
         JFrame window = new JFrame("Chess"); // Create new window
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Close the program when the window is closed
         window.setResizable(false); // Window is not resizable
+
+        GamePanel gp = new GamePanel(); // Initialize a new GamePanel
+        window.add(gp); // Add GamePanel to the window
+        window.pack(); // Adjust the window size to the panel
+
         window.setLocationRelativeTo(null); // Window will appear in the center of the monitor
         window.setVisible(true); // Window is visible on the screen
+
+        gp.launchGame(); // Lauch Chess
     }
 }
